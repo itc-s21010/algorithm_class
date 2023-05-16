623f8d55b8e95901a35459564f1717f1c505d98e
s21010 <s21010@std.it-college.ac.jp>
2023-05-16 11:55:28 +0900

    5/16 課題

--- ex_lesson2/prime_3_9*9.py
+++ ex_lesson2/prime_3_9*9.py
@@ -1,26 +1,25 @@
-p = [True]*100
+p = [True]*82
 p[0] = False
 p[1] = False
 n = 2
-
 def hyou():
     s = ""
-    for i in range(100):
+    for i in range(82):
         if p[i] == True:
             s += "{:2d}, ".format(i)
         else:
             s += "/, "
-       if i%10 == 9:
-           s += "¥n"
+        if i%10 == 9:
+            s += "\n"
     print(s)
 
 def furui():
     global n
-    for i in range(n+n, 100, n):
+    for i in range(n+n, 82, n):
         p[i] = False
-    print(n, "の倍数を篩い落としました。")
-    hyou() 
-    while n<100: # 次に篩い落とす数
+    print(n, "の倍数をふるい落としました。")
+    hyou()
+    while n<82:
         n = n + 1
         if p[n] == True:
             break
