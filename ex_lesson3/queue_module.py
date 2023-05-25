7314f378b7c501b84f94df9aced1d08dff3bea1a
s21010 <s21010@std.it-college.ac.jp>
2023-05-25 10:09:30 +0900

    5/25

--- ex_lesson3/queue_module.py
+++ ex_lesson3/queue_module.py
@@ -1,9 +1,9 @@
 import queue
 
-MAX = 10
+MAX = 20
 
 print("キュー")
-q = queue.Queue
+q = queue.Queue()
 for i in range(MAX):
     q.put(i)
 for i in range(MAX):
@@ -15,4 +15,4 @@ s = queue.LifoQueue()
 for i in range(MAX):
     s.put(i)
 for i in range(MAX):
-    print(s.get(),end="から")
+    print(s.get(), end="から")
